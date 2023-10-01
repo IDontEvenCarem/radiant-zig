a1ecb46e7447132cb75ec12efae274985efe07b4
Kajetan Owczarek <flash3ch@gmail.com>
2023-10-01 18:10:45 +0200

    Reading in files

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,65 @@
 const std = @import("std");
 
-const Token = enum {
+const Token = struct {
+
+};
+
+const ParseContext = struct {
+    alloc: std.mem.Allocator,
     
+    pub fn init (alloc: std.mem.Allocator) ParseContext {
+        return ParseContext{
+            .alloc = alloc
+        };
+    }
+
+    pub fn readText(text: []u8) void {
+        _ = text;
+    }
+
+    pub fn getTokens() []Token {
+        
+    }
 };
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpio = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpio.deinit();
+    var alloc = gpio.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    var args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
+
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var parseCtx = ParseContext.init(alloc);
+    _ = parseCtx;
+    if (args.len == 1) {
+        // run the REPL
+    }
+    else if (args.len == 2) {
+        // open the file
+        var filename = args[1];
+        var fileContent = try readRelativeFile(alloc, filename);
+        defer alloc.free(fileContent);
+        try stdout.print("Contents: \n{s}", .{fileContent});
+    }
+    else {
+        _ = std.io.getStdOut().write("Usage: radiant [source]") catch return;
+    }
 
-    try bw.flush(); // don't forget to flush!
+    try bw.flush();
+}
+
+fn readRelativeFile(alloc: std.mem.Allocator, relFilename: []const u8) ![]u8 {
+    var absPath = try std.fs.cwd().realpathAlloc(alloc, relFilename);
+    defer alloc.free(absPath);
+    var file = try std.fs.openFileAbsolute(absPath, std.fs.File.OpenFlags{});
+    defer file.close();
+    var fileBuff = try file.readToEndAlloc(alloc, 268435456);
+    return fileBuff;
 }
 
 test "simple test" {
